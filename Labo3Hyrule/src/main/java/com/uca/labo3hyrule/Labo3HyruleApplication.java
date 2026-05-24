052457df696973c122d34ab528d807921cc5e824
eliuthlarios <00029223@uca.edu.sv>
2026-05-23 22:04:50 -0600

    estructura de carpetas

---
+++ Labo3Hyrule/src/main/java/com/uca/labo3hyrule/Labo3HyruleApplication.java
@@ -0,0 +1,13 @@
+package com.uca.labo3hyrule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo3HyruleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labo3HyruleApplication.class, args);
+    }
+
+}
