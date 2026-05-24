6c2faa50739baef1a19e83df1feff05896e5be32
eliuthlarios <00029223@uca.edu.sv>
2026-05-23 22:13:09 -0600

    :white_check_mark: archivos iniciales

---
+++ Labo3Hyrule/src/main/java/com/uca/labo3hyrule/domain/dto/response/SpecimenResponse.java
@@ -0,0 +1,4 @@
+package com.uca.labo3hyrule.domain.dto.response;
+
+public class SpecimenResponse {
+}
---
+++ Labo3Hyrule/src/main/java/com/uca/labo3hyrule/services/impl/SpecimenServiceImpl.java
@@ -0,0 +1,55 @@
+package com.uca.labo3hyrule.services.impl;
+
+import com.uca.labo3hyrule.domain.dto.response.SpecimenResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SpecimenServiceImpl implements SpecimenService {
+    private final SpecimenRepository specimenRepository;
+    private final SpecimenMapper specimenMapper;
+
+    @Override
+    @Transactional
+    public SpecimenResponse createSpecimen(CreateSpecimenRequest request) {
+        return specimenMapper.toDto(
+                specimenRepository.save(specimenMapper.toEntity(request))
+        );
+    }
+
+    @Override
+    public List<SpecimenResponse> getAllSpecimens() {
+        List<Specimen> specimens = specimenRepository.findAll();
+        if (specimens.isEmpty())
+            throw new ResourceNotFoundException("No specimens are registered in Hyrule");
+
+        return specimens.stream()
+                .map(specimenMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public SpecimenResponse getSpecimenById(UUID id) {
+        return specimenMapper.toDto(specimenRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Specimen not found in Hyrule Records"))
+        );
+    }
+
+    @Override
+    @Transactional
+    public SpecimenResponse updateSpecimen(UUID id, UpdateSpecimenRequest request) {
+        this.getSpecimenById(id);
+        return specimenMapper.toDto(specimenRepository.save(specimenMapper.toEntityUpdate(request, id)));
+    }
+
+    @Transactional
+    public SpecimenResponse deleteSpecimen(UUID id) {
+        SpecimenResponse existSpecimen = this.getSpecimenById(id);
+        specimenRepository.deleteById(id);
+        return existSpecimen;
+    }
+}
